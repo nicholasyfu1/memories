e6fdb24a6b381fd0a12c66ef9bb550b085a5a133
nicholasyfu1 <nicholasyfu1@gmail.com>
2022-10-12 17:45:10 -0700

    syntax

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
