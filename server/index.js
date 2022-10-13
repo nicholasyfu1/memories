1e15bd48e60512e03b45e0549c21b95ead0dd2ab
nicholasyfu1 <nicholasyfu1@gmail.com>
2022-10-12 17:42:14 -0700

    Deployment

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello API');
+});
 
 const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
